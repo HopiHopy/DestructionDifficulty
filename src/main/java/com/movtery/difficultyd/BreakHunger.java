bf13cd9fe4de35c355be372db431f4fdf7fa11ce
MovTery <123115192+HopiHopy@users.noreply.github.com>
2023-12-11 23:47:57 +0800

    1.5.3

--- src/main/java/com/movtery/difficultyd/BreakHunger.java
+++ src/main/java/com/movtery/difficultyd/BreakHunger.java
@@ -3,6 +3,7 @@ package com.movtery.difficultyd;
 import com.movtery.difficultyd.Config.ModAutoConfig;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
 
 import java.util.Random;
 
@@ -14,6 +15,9 @@ public class BreakHunger implements ModInitializer {
         if (!(foodLevel == 0) && chance < config.foodLevelChance) {
             int newValue = foodLevel - config.removeFoodLevel;
             player.getHungerManager().setFoodLevel(Math.max(newValue, 0));
+            actionbarYes(config, chance, config.foodLevelChance, player);
+        } else {
+            actionbarNo(config, chance, config.foodLevelChance, player);
         }
     }
 
@@ -24,6 +28,30 @@ public class BreakHunger implements ModInitializer {
         if (!(foodLevel == 0) && chance < config.exhaustionLevelChance) {
             float newValue = Exhaustionlevel + config.addExhaustionLevel;
             player.getHungerManager().setExhaustion(newValue);
+            actionbarYes(config, chance, config.exhaustionLevelChance, player);
+        } else {
+            actionbarNo(config, chance, config.exhaustionLevelChance, player);
+        }
+    }
+
+    private static void actionbarYes(ModAutoConfig config, float chance1, float chance2, PlayerEntity player) {
+        if (config.actionbarFoodLevelChance) {
+            Text message = Text.translatable("text.difficultyd.randomnumbers")
+                    .append(String.valueOf(chance1))
+                    .append(" < ")
+                    .append(String.valueOf(chance2))
+                    .append(Text.translatable("text.difficultyd.randomnumbers.yes"));
+            player.sendMessage(message, true);
+        }
+    }
+    private static void actionbarNo(ModAutoConfig config, float chance1, float chance2, PlayerEntity player) {
+        if (config.actionbarFoodLevelChance) {
+            Text message = Text.translatable("text.difficultyd.randomnumbers")
+                    .append(String.valueOf(chance1))
+                    .append(" > ")
+                    .append(String.valueOf(chance2))
+                    .append(Text.translatable("text.difficultyd.randomnumbers.no"));
+            player.sendMessage(message, true);
         }
     }
 
--- src/main/java/com/movtery/difficultyd/DifficultyD.java
+++ src/main/java/com/movtery/difficultyd/DifficultyD.java
@@ -21,86 +21,101 @@ import net.minecraft.world.World;
 import java.util.Random;
 
 public class DifficultyD implements ModInitializer {
-    private void setAir(World world, BlockPos pos) {
-        world.setBlockState(pos, Blocks.AIR.getDefaultState(), Block.NOTIFY_LISTENERS | Block.REDRAW_ON_MAIN_THREAD);
-    }
     private final Random rand = new Random();
-    private void actionbar(ModAutoConfig config, float chance1, String string, float chance2, String string2, PlayerEntity player) {
-        if (config.actionbarChance) {
-            Text message = Text.translatable("text.difficultyd.randomnumbers")
-                    .append(String.valueOf(chance1))
-                    .append(string)
-                    .append(String.valueOf(chance2))
-                    .append(Text.translatable(string2));
-            player.sendMessage(message, true);
-        }
-    }
+
     @Override
     public void onInitialize() {
         AutoConfig.register(ModAutoConfig.class, JanksonConfigSerializer::new);
         ModAutoConfig config = AutoConfig.getConfigHolder(ModAutoConfig.class).getConfig();
 
         PlayerBlockBreakEvents.BEFORE.register((world, player, pos, state, entity) -> {
-            if (!config.disable && config.foodLevel && !(player.isCreative())) {
-                BreakHunger.setFoodLevel(player, config);
-            }
-            if (!config.disable && config.exhaustionLevel && !(player.isCreative())) {
-                BreakHunger.setExhaustionLevel(player, config);
-            }
+            if (!config.disable && !(player.isCreative())) {
 
-            ItemStack heldItem = player.getMainHandStack();
-            Item item = heldItem.getItem();
-            String blockName;
-            if (config.blockWhitelistDisable) {
-                blockName = Registry.BLOCK.getId(state.getBlock()).toString();
-            } else {
-                blockName = Blocks.AIR.toString();
-            }
+                ItemStack heldItem = player.getMainHandStack();
+                Item item = player.getMainHandStack().getItem();
+                String blockName;
+                float chance = rand.nextFloat() * 100;
 
-            float chance = rand.nextFloat() * 100;
-            if (!config.disable && !config.blockWhitelist.contains(blockName) && !(player.isCreative())) {
-
-                if (item instanceof MiningToolItem) {
-                    if (EnchantmentHelper.getLevel(Enchantments.SILK_TOUCH, heldItem) > 0) {
-                        if (chance > config.silkTouchChance) {
-                            setAir(world, pos);
-                            actionbar(config, chance, " > ", config.silkTouchChance, "text.difficultyd.randomnumbers.no", player);
-                        } else {
-                            actionbar(config, chance, " < ", config.silkTouchChance, "text.difficultyd.randomnumbers.yes", player);
-                        }
-                        return true;
-                    }
-                    if (EnchantmentHelper.getLevel(Enchantments.FORTUNE, heldItem) > 0) {
-                        if (chance > config.fortuneChance) {
-                            setAir(world, pos);
-                            actionbar(config, chance, " > ", config.fortuneChance, "text.difficultyd.randomnumbers.no", player);
-                        } else {
-                            actionbar(config, chance, " < ", config.fortuneChance, "text.difficultyd.randomnumbers.yes", player);
-                        }
-                        return true;
-                    }
-                    if (chance > config.normalChance) {
-                        setAir(world, pos);
-                        actionbar(config, chance, " > ", config.normalChance, "text.difficultyd.randomnumbers.no", player);
-                    } else {
-                        actionbar(config, chance, " < ", config.normalChance, "text.difficultyd.randomnumbers.yes", player);
-                    }
-                    return true;
+                if (config.foodLevel) {
+                    BreakHunger.setFoodLevel(player, config);
+                }
+                if (config.exhaustionLevel) {
+                    BreakHunger.setExhaustionLevel(player, config);
+                }
+                if (config.blockWhitelistDisable) {
+                    blockName = Registry.BLOCK.getId(state.getBlock()).toString();
                 } else {
-                    if (!config.emptyHanded) {
-                        if (chance > config.emptyHandedChance) {
+                    blockName = Blocks.AIR.toString();
+                }
+
+                if (!config.blockWhitelist.contains(blockName)) {
+                    if (item instanceof MiningToolItem) {
+                        if (EnchantmentHelper.getLevel(Enchantments.SILK_TOUCH, heldItem) > 0) {
+                            if (chance > config.silkTouchChance) {
+                                setAir(world, pos);
+                                actionbarNo(config, chance,  config.silkTouchChance, player);
+                            } else {
+                                actionbarYes(config, chance,  config.silkTouchChance, player);
+                            }
+                            return true;
+                        }
+                        if (EnchantmentHelper.getLevel(Enchantments.FORTUNE, heldItem) > 0) {
+                            if (chance > config.fortuneChance) {
+                                setAir(world, pos);
+                                actionbarNo(config, chance,  config.fortuneChance, player);
+                            } else {
+                                actionbarYes(config, chance,  config.fortuneChance, player);
+                            }
+                            return true;
+                        }
+                        if (chance > config.normalChance) {
                             setAir(world, pos);
-                            actionbar(config, chance, " > ", config.emptyHandedChance, "text.difficultyd.randomnumbers.no", player);
+                            actionbarNo(config, chance,  config.normalChance, player);
                         } else {
-                            actionbar(config, chance, " < ", config.emptyHandedChance, "text.difficultyd.randomnumbers.yes", player);
+                            actionbarYes(config, chance,  config.normalChance, player);
                         }
                         return true;
                     } else {
-                        setAir(world, pos);
+                        if (!config.emptyHanded) {
+                            if (chance > config.emptyHandedChance) {
+                                setAir(world, pos);
+                                actionbarNo(config, chance,  config.emptyHandedChance, player);
+                            } else {
+                                actionbarYes(config, chance,  config.emptyHandedChance, player);
+                            }
+                            return true;
+                        } else {
+                            setAir(world, pos);
+                        }
                     }
                 }
             }
             return true;
         });
+    }
+
+    private void setAir(World world, BlockPos pos) {
+        world.setBlockState(pos, Blocks.AIR.getDefaultState(), Block.NOTIFY_LISTENERS | Block.REDRAW_ON_MAIN_THREAD);
+    }
+
+    private void actionbarYes(ModAutoConfig config, float chance1, float chance2, PlayerEntity player) {
+        if (config.actionbarChance) {
+            Text message = Text.translatable("text.difficultyd.randomnumbers")
+                    .append(String.valueOf(chance1))
+                    .append(" < ")
+                    .append(String.valueOf(chance2))
+                    .append(Text.translatable("text.difficultyd.randomnumbers.yes"));
+            player.sendMessage(message, true);
+        }
+    }
+    private void actionbarNo(ModAutoConfig config, float chance1, float chance2, PlayerEntity player) {
+        if (config.actionbarChance) {
+            Text message = Text.translatable("text.difficultyd.randomnumbers")
+                    .append(String.valueOf(chance1))
+                    .append(" > ")
+                    .append(String.valueOf(chance2))
+                    .append(Text.translatable("text.difficultyd.randomnumbers.no"));
+            player.sendMessage(message, true);
+        }
     }
 }
